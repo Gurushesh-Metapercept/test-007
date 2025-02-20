eeaf0559a3bc5afcd735a2a83d239464ddb94d21
Gurushesh-Metapercept <gurushesh.p@metapercept.com>
2025-02-20 16:58:36 +0530

    Log updater and read source files logic

--- utils/processDataset.js
+++ utils/processDataset.js
@@ -1,5 +1,6 @@
 const fs = require("fs").promises;
 const path = require("path");
+const cheerio = require("cheerio");
 const { addLogData } = require("../state/logData");
 
 const LOG_FILE = "process_log.log";
@@ -30,7 +31,30 @@ async function processDataset(dataset) {
       conrefID && conrefID.includes("\\") ? conrefID.split("\\")[1] : conrefID;
 
     const cleanPath = relativePath.split("#")[0];
-    console.log("👉 [processDataset.js:45]: cleanPath: ", cleanPath);
+
+    if (item.p_id !== "N/A") {
+      try {
+        try {
+          await fs.access(cleanPath);
+        } catch (err) {
+          const errorMsg = `File not found at ${cleanPath} -- from: ${
+            ("/", item.from.split("input\\")[1])
+          }`;
+          await logMessage(errorMsg, true);
+          continue;
+        }
+
+        let fileContent = await fs.readFile(cleanPath, "utf-8");
+        const $ = cheerio.load(fileContent, { xmlMode: true });
+        // console.log("👉 [processDataset.js:60]: $: ", $.html());
+
+        const updateMsg = `Updated: ${cleanPath}`;
+        await logMessage(updateMsg);
+      } catch (err) {
+        const errorMsg = `Error processing ${cleanPath}: ${err.message}`;
+        await logMessage(errorMsg, true);
+      }
+    }
   }
 }
 
